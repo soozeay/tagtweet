9bdabf296603ee63d2e2468ba046ddccfd89ee13
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-09 19:26:27 +0900

    タグを表示させる

--- app/javascript/tag.js
+++ app/javascript/tag.js
@@ -4,13 +4,22 @@ if (location.pathname.match("tweets/new")){
     inputElement.addEventListener("keyup", () => {
       const keyword = document.getElementById("tweets_tag_name").value;
       const XHR = new XMLHttpRequest();
-      XHR.open("GET", `search/?keyword=${keyword}`, true); 
+      XHR.open("GET", `search/?keyword=${keyword}`, true);
       //openメソッドの第一引数にHTTPメソッド、第二引数にURL、第三引数には非同期通信
       XHR.responseType = "json";
       XHR.send();
       XHR.onload = () => {
         const tagName = XHR.response.keyword;
+        const searchResult = document.getElementById("search-result");
+        tagName.forEach((tag) => {
+          const childElement = document.createElement("div");
+          childElement.setAttribute("class", "child");
+          childElement.setAttribute("id", tag.id);
+          childElement.innerHTML = tag.name;
+          searchResult.appendChild(childElement);
+          });
+        });
       };
     });
   });
-}
+};
