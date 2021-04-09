de1a6afeeac60b2b6b88cb2a99214e498336bfe7
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-09 17:48:42 +0900

    XMLHttpRequestオブジェクトを生成し、openメソッドでリクエストを定義

--- app/javascript/tag.js
+++ app/javascript/tag.js
@@ -3,6 +3,10 @@ if (location.pathname.match("tweets/new")){
     const inputElement = document.getElementById("tweets_tag_name");
     inputElement.addEventListener("keyup", () => {
       const keyword = document.getElementById("tweets_tag_name").value;
+      const XHR = new XMLHttpRequest();
+      XHR.open("GET", `search/?keyword=${keyword}`, true); 
+      //openメソッドの第一引数にHTTPメソッド、第二引数にURL、第三引数には非同期通信
+      
     });
   });
 }
