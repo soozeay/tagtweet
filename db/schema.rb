9a32da6484deed713774b5ac5e6015512ee0ea35
soozeay <80019801+soozeay@users.noreply.github.com>
2021-04-09 16:13:52 +0900

    Tagモデル生成
    migrate済み

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_09_071102) do
+ActiveRecord::Schema.define(version: 2021_04_09_071312) do
+
+  create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "message", null: false
